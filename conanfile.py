55ea347355d774c54dde01143e1b6fbda99e0fb8
Steve Mokris <smokris@softpixel.com>
2018-04-06 15:22:32 -0400

    Support building on Linux.

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
+import platform
 import shutil
 
 class LiblqrConan(ConanFile):
@@ -8,7 +9,8 @@ class LiblqrConan(ConanFile):
     package_version = '4'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'glib/2.51.1-3@vuo/stable'
+    build_requires = 'llvm/3.3-5@vuo/stable'
+    requires = 'glib/2.51.1-4@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-liblqr'
     license = 'https://github.com/carlobaldassi/liblqr/blob/master/COPYING.LESSER'
@@ -16,25 +18,42 @@ class LiblqrConan(ConanFile):
     source_dir = 'liblqr-%s' % source_version
     build_dir = '_build'
 
+    def requirements(self):
+        if platform.system() == 'Linux':
+            self.requires('patchelf/0.10pre-1@vuo/stable')
+        elif platform.system() != 'Darwin':
+            raise Exception('Unknown platform "%s"' % platform.system())
+
+    def imports(self):
+        self.copy('*', self.build_dir, 'lib')
+
     def source(self):
         tools.get('https://github.com/carlobaldassi/liblqr/archive/v%s.tar.gz' % self.source_version,
                   sha256='1019a2d91f3935f1f817eb204a51ec977a060d39704c6dafa183b110fd6280b0')
 
         self.run('mv %s/COPYING.LESSER %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
 
-    def imports(self):
-        self.copy('*.dylib', self.build_dir, 'lib')
-
     def build(self):
         tools.mkdir(self.build_dir)
         with tools.chdir(self.build_dir):
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.flags.append('-Oz')
-            autotools.flags.append('-mmacosx-version-min=10.10')
-            autotools.link_flags.append('-Wl,-rpath,@loader_path')
-            autotools.link_flags.append('-Wl,-install_name,@rpath/liblqr.dylib')
 
-            env_vars = {'PKG_CONFIG_PATH': self.deps_cpp_info["glib"].rootpath}
+            # The LLVM/Clang libs get automatically added by the `requires` line,
+            # but this package doesn't need to link with them.
+            autotools.libs = ['c++abi']
+
+            autotools.flags.append('-Oz')
+
+            if platform.system() == 'Darwin':
+                autotools.flags.append('-mmacosx-version-min=10.10')
+                autotools.link_flags.append('-Wl,-rpath,@loader_path')
+                autotools.link_flags.append('-Wl,-install_name,@rpath/liblqr.dylib')
+
+            env_vars = {
+                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
+                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
+                'PKG_CONFIG_PATH': self.deps_cpp_info["glib"].rootpath,
+            }
             with tools.environment_append(env_vars):
                 autotools.configure(configure_dir='../%s' % self.source_dir,
                                     args=['--quiet',
@@ -42,11 +61,24 @@ class LiblqrConan(ConanFile):
 
                 autotools.make(args=['install'])
 
-            shutil.move('lqr/.libs/liblqr-1.0.dylib', 'lqr/.libs/liblqr.dylib')
- 
+            if platform.system() == 'Darwin':
+                shutil.move('lqr/.libs/liblqr-1.0.dylib', 'lqr/.libs/liblqr.dylib')
+            elif platform.system() == 'Linux':
+                self.run('ls -lR')
+                shutil.move('lqr/.libs/liblqr.so.0.1.0', 'lqr/.libs/liblqr.so')
+                patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
+                self.run('%s --set-soname liblqr.so lib/liblqr.so' % patchelf)
+
     def package(self):
+        if platform.system() == 'Darwin':
+            libext = 'dylib'
+        elif platform.system() == 'Linux':
+            libext = 'so'
+        else:
+            raise Exception('Unknown platform "%s"' % platform.system())
+
         self.copy('*.h', src='%s/include/lqr-1' % self.build_dir, dst='include')
-        self.copy('liblqr.dylib', src='%s/lqr/.libs' % self.build_dir, dst='lib')
+        self.copy('liblqr.%s' % libext, src='%s/lqr/.libs' % self.build_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,19 +1,27 @@
 from conans import ConanFile
+import platform
 
 class LiblqrTestConan(ConanFile):
+    requires = 'llvm/3.3-5@vuo/stable'
     generators = 'qbs'
 
     def build(self):
         self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
-        self.copy('*.dylib', dst='bin', src='lib')
+        self.copy('*', src='bin', dst='bin')
+        self.copy('*', src='lib', dst='lib')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs run -f "%s"' % self.source_folder)
 
-        # Ensure we only link to system libraries and to libraries we built.
-        self.run('! (otool -L bin/liblqr.dylib | tail +3 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
-
-        # Ensure we don't search any absolute rpaths.
-        self.run('! (otool -l bin/liblqr.dylib | grep -A2 LC_RPATH | grep path | cut -b 15- | cut -d"(" -f1 | egrep -v "^[^/]")')
+        # Ensure we only link to system libraries.
+        if platform.system() == 'Darwin':
+            self.run('! (otool -L lib/liblqr.dylib | tail +3 | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/liblqr.dylib | fgrep "libstdc++")')
+            self.run('! (otool -l lib/liblqr.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
+        elif platform.system() == 'Linux':
+            self.run('! (ldd lib/liblqr.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
+            self.run('! (ldd lib/liblqr.so | fgrep "libstdc++")')
+        else:
+            raise Exception('Unknown platform "%s"' % platform.system())
