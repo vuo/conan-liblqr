6238771c0d26e996ba846c481811d2e70713ebed
Steve Mokris <steve@kosada.com>
2020-11-07 20:59:49 -0500

    Build for both x86_64 + arm64.  [kosada/vuo/vuo#17836]

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
+import os
 import platform
 import shutil
 
@@ -6,17 +7,25 @@ class LiblqrConan(ConanFile):
     name = 'liblqr'
 
     source_version = '0.4.2'
-    package_version = '4'
+    package_version = '5'
     version = '%s-%s' % (source_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable'
-    requires = 'glib/2.51.1-4@vuo/stable'
+    build_requires = (
+        'llvm/5.0.2-1@vuo/stable',
+        'macos-sdk/11.0-0@vuo/stable',
+    )
+    requires = 'glib/2.66.2-0@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
-    url = 'https://github.com/vuo/conan-liblqr'
+    url = 'https://github.com/carlobaldassi/liblqr'
     license = 'https://github.com/carlobaldassi/liblqr/blob/master/COPYING.LESSER'
     description = 'A C/C++ API for performing non-uniform resizing of images by the seam-carving technique'
     source_dir = 'liblqr-%s' % source_version
-    build_dir = '_build'
+
+    build_x86_dir = '_build_x86'
+    build_arm_dir = '_build_arm'
+    install_x86_dir = '_install_x86'
+    install_arm_dir = '_install_arm'
+    install_universal_dir = '_install_universal_dir'
 
     def requirements(self):
         if platform.system() == 'Linux':
@@ -25,7 +34,8 @@ class LiblqrConan(ConanFile):
             raise Exception('Unknown platform "%s"' % platform.system())
 
     def imports(self):
-        self.copy('*', self.build_dir, 'lib')
+        self.copy('*', self.build_x86_dir, 'lib')
+        self.copy('*', self.build_arm_dir, 'lib')
 
     def source(self):
         tools.get('https://github.com/carlobaldassi/liblqr/archive/v%s.tar.gz' % self.source_version,
@@ -34,39 +44,67 @@ class LiblqrConan(ConanFile):
         self.run('mv %s/COPYING.LESSER %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
 
     def build(self):
-        tools.mkdir(self.build_dir)
-        with tools.chdir(self.build_dir):
-            autotools = AutoToolsBuildEnvironment(self)
+        autotools = AutoToolsBuildEnvironment(self)
 
-            # The LLVM/Clang libs get automatically added by the `requires` line,
-            # but this package doesn't need to link with them.
-            autotools.libs = ['c++abi']
+        # The LLVM/Clang libs get automatically added by the `requires` line,
+        # but this package doesn't need to link with them.
+        autotools.libs = []
 
-            autotools.flags.append('-Oz')
+        autotools.flags.append('-Oz')
 
-            if platform.system() == 'Darwin':
-                autotools.flags.append('-mmacosx-version-min=10.10')
-                autotools.link_flags.append('-Wl,-rpath,@loader_path')
-                autotools.link_flags.append('-Wl,-install_name,@rpath/liblqr.dylib')
+        if platform.system() == 'Darwin':
+            autotools.flags.append('-isysroot %s' % self.deps_cpp_info['macos-sdk'].rootpath)
+            autotools.flags.append('-mmacosx-version-min=10.11')
+            autotools.link_flags.append('-Wl,-rpath,@loader_path')
+            autotools.link_flags.append('-Wl,-install_name,@rpath/liblqr.dylib')
 
-            env_vars = {
-                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
-                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
-                'PKG_CONFIG_PATH': self.deps_cpp_info["glib"].rootpath,
-            }
-            with tools.environment_append(env_vars):
+        common_configure_args = [
+            '--quiet',
+            '--disable-dependency-tracking',
+            '--disable-static',
+            '--enable-shared',
+        ]
+
+        env_vars = {
+            'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
+            'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
+            'PKG_CONFIG': 'false',
+            'GLIB_CFLAGS': '-I%s' % self.deps_cpp_info['glib'].include_paths[0],
+            'GLIB_LIBS': '-lglib',
+        }
+        with tools.environment_append(env_vars):
+            build_root = os.getcwd()
+
+            self.output.info("=== Build for x86_64 ===")
+            tools.mkdir(self.build_x86_dir)
+            with tools.chdir(self.build_x86_dir):
+                autotools.flags.append('-arch x86_64')
+                autotools.link_flags.append('-arch x86_64')
                 autotools.configure(configure_dir='../%s' % self.source_dir,
-                                    args=['--quiet',
-                                          '--prefix=%s/%s' % (self.build_folder, self.build_dir)])
+                                    build=False,
+                                    host=False,
+                                    args=common_configure_args + [
+                                        '--prefix=%s/%s' % (build_root, self.install_x86_dir),
+                                    ])
+                autotools.make(args=['--quiet'])
+                autotools.make(target='install', args=['--quiet'])
 
-                autotools.make(args=['install'])
-
-            if platform.system() == 'Darwin':
-                shutil.move('lqr/.libs/liblqr-1.0.dylib', 'lqr/.libs/liblqr.dylib')
-            elif platform.system() == 'Linux':
-                shutil.move('lib/liblqr-1.so.0.3.2', 'lib/liblqr.so')
-                patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
-                self.run('%s --set-soname liblqr.so lib/liblqr.so' % patchelf)
+            self.output.info("=== Build for arm64 ===")
+            tools.mkdir(self.build_arm_dir)
+            with tools.chdir(self.build_arm_dir):
+                autotools.flags.remove('-arch x86_64')
+                autotools.flags.append('-arch arm64')
+                autotools.link_flags.remove('-arch x86_64')
+                autotools.link_flags.append('-arch arm64')
+                autotools.configure(configure_dir='../%s' % self.source_dir,
+                                    build=False,
+                                    host=False,
+                                    args=common_configure_args + [
+                                        '--prefix=%s/%s' % (build_root, self.install_arm_dir),
+                                        '--host=x86_64-apple-darwin15.0.0',
+                                    ])
+                autotools.make(args=['--quiet'])
+                autotools.make(target='install', args=['--quiet'])
 
     def package(self):
         if platform.system() == 'Darwin':
@@ -76,9 +114,12 @@ class LiblqrConan(ConanFile):
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
 
-        self.copy('*.h', src='%s/include/lqr-1' % self.build_dir, dst='include')
-        self.copy('liblqr.%s' % libext, src='%s/lqr/.libs' % self.build_dir, dst='lib')
-        self.copy('liblqr.%s' % libext, src='%s/lib' % self.build_dir, dst='lib')
+        tools.mkdir(self.install_universal_dir)
+        with tools.chdir(self.install_universal_dir):
+            self.run('lipo -create ../%s/lib/liblqr-1.%s ../%s/lib/liblqr-1.%s -output liblqr.%s' % (self.install_x86_dir, libext, self.install_arm_dir, libext, libext))
+
+        self.copy('*.h', src='%s/include/lqr-1' % self.install_x86_dir, dst='include')
+        self.copy('liblqr.%s' % libext, src=self.install_universal_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
